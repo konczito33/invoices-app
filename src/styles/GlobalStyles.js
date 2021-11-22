e75b87ec9f66de0745749955b8384b744bc6c20f
Hubert Klauzinski <konczito33@gmail.com>
2021-11-22 23:49:05 +0100

    aadwasdw

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -1,6 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
 export const GlobalStyles = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Spartan:wght@400;600&display=swap');
 *{
     box-sizing: border-box;
 }
